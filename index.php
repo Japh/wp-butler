d701bc21cc0eb225348ab8f75bbbb86915a6168e
Japh <japhie@gmail.com>
2012-12-24 02:31:37 +1100

    Included Mousetrap and implemented keyboard shortcut modal

--- index.php
+++ index.php
@@ -13,24 +13,25 @@ Version: 1.0
 Author URI: http://www.jordesign.com
 */
 
-add_action( 'wp_dashboard_setup', 'jg_butler_setup' );
-
-function jg_butler_setup() {
-	global $wp_meta_boxes;
-	wp_add_dashboard_widget( 'wp_butler', 'WP Butler', 'jg_wp_butler' );
-}
+add_action( 'admin_footer', 'jg_wp_butler' );
 
 function jg_wp_butler() {
-	echo '<p style="padding: 5px 0px; margin:0 0 10px 0; color: #8F8F8F;font-size: 14px;border-bottom:1px solid #ddd;display:inline-block;">What would you like to do?</p>';
-	echo '<form id="wpButler"><input type="text" placeholder="just start typing..." style="width:90%; font-size:16px;padding:4px 0 6px 10px"id="wpButlerField"><input type="hidden" id="wp-butler-nonce" name="wp-butler-nonce" value="' . wp_create_nonce( 'wp_butler_nonce' ) . '" /></form>';
+	echo '<div id="butler-dialog" title="WP Butler" style="display: none;">
+	<a id="butler-close-dialog" class="media-modal-close" href="#" title="Close"><span class="media-modal-icon"></span></a>
+	<p>
+		<p class="butler-question">What would you like to do?</p>
+		<form id="wpButler"><input type="text" placeholder="Just start typing..." style="width:90%; font-size:16px;padding:4px 0 6px 10px"id="wpButlerField"><input type="hidden" id="wp-butler-nonce" name="wp-butler-nonce" value="' . wp_create_nonce( 'wp_butler_nonce' ) . '" /></form>
+	</p>
+</div>';
 }
 
 // Enqueue jQuery UI Autocomplete
 function jg_add_autocomplete() {
 	if ( is_admin() ) {
-		wp_register_script( 'wpbutler', plugins_url( 'wpbutler.js', __FILE__ ), array( 'jquery', 'jquery-ui-autocomplete' ), '1.0', true );
-		//wp_enqueue_script('jquery-ui-autocomplete', '', array('jquery-ui-widget', 'jquery-ui-position'), '1.8.6');
-		wp_enqueue_script( 'wpbutler' );
+		wp_register_script( 'mousetrap', plugins_url( 'mousetrap/mousetrap.min.js', __FILE__ ), array(), '1.2.2', true );
+		wp_enqueue_script( 'wpbutler', plugins_url( 'wpbutler.js', __FILE__ ), array( 'jquery', 'jquery-ui-autocomplete', 'jquery-ui-dialog', 'mousetrap' ), '1.0', true );
+		wp_enqueue_style( 'wp-jquery-ui-dialog' );
+		wp_enqueue_style( 'wpbutler', plugins_url( 'wpbutler.css', __FILE__ ) );
 	}
 };
 
--- wpbutler.js
+++ wpbutler.js
@@ -1,5 +1,22 @@
 // WP Butler
 jQuery( function($) {
+
+	Mousetrap.bind( 'shift+alt+b', function(e) {
+		$( "#butler-dialog" ).dialog({
+			modal: true,
+			closeOnEscape: true
+		});
+
+		$( "#wpButlerField" ).focus();
+
+		return false;
+	}, 'keyup' );
+
+	$( "#butler-close-dialog" ).click(function(e) {
+		e.preventDefault();
+		$( "#butler-dialog" ).dialog( "close" );
+	});
+
 	$( "#wpButlerField" ).autocomplete({
 		source: ajaxurl + '?action=wp_butler_actions&_nonce=' + $.trim( $('#wp-butler-nonce').val() ),
 		select: function( event , ui ) {
